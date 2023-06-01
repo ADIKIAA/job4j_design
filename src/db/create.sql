3ccfe4c4c1f88190d738288199b122823faf1d3c
ADIKIAA <zhekov.nick@yandex.ru>
2023-06-01 15:50:29 +0300

    2. Схема прав пользователи и роли. [#1093 #420124]

--- src/db/create.sql
+++ src/db/create.sql
@@ -18,12 +18,14 @@ create table roles_rules(
     id serial primary key,
     role_id int references roles(id),
     rule_id int references rules(id)
-)
+);
 
 create table items(
     id serial primary key,
     name text,
-    user_id int references users(id)
+    user_id int references users(id),
+    category_id int references categories(id),
+    state_id int references states(id)
 );
 
 create table comments(
@@ -40,14 +42,12 @@ create table attachs(
 
 );
 
-reate table categories(
+create table categories(
     id serial primary key,
-    name text,
-    item_id int references items(id)
+    name text
 );
 
 create table states(
     id serial primary key,
-    name text,
-    item_id int references items(id)
+    name text
 );
