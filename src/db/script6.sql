9204971956a7fb3a5adfa8a76f878905e60a2e0a
ADIKIAA <zhekov.nick@yandex.ru>
2023-06-07 15:54:45 +0300

    1. Представления [#504792]

---
+++ src/db/script6.sql
@@ -0,0 +1,53 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+create view bestseller as
+select a.id, a.name "автор", count(*) "количество заказов" from orders o 
+join students s on (o.student_id = s.id) 
+join books b on (o.book_id = b.id) 
+join authors a on (a.id = b.author_id) 
+group by a.id, a.name
+having count(*) > 2;
+
+	
+	
+	
+	
